2b0d81b413baf7a5d58fc1186e1bf2cd2758efb6
Antonio Rafael Xavier de Ameida <antonio.xavier@equatorialenergia.com.br>
2024-03-20 18:22:51 +0000

    Criação da DIM_CLIENTES

---
+++ models/staging/sap_adventure_works/stg_sap_adw__customer.sql
@@ -0,0 +1,16 @@
+/*Criação da staging: stg_sap_adw__customer.sql*/
+/*1º CET, customer_adw, tabela com as chaves primárias e estrangeiras pertinentes aos clientes da Adventure_Works*/
+with customer_adw as (
+    select 
+        CUSTOMERID as PK_ID_CLIENTE,
+        PERSONID as FK_ID_REG_PESSOA,
+        STOREID as FK_ID_LOJA,
+        TERRITORYID as FK_ID_TERRITORIO
+
+    from {{ source('sap_adw', 'customer') }}
+)
+
+/*2º resultado final, Dcustomer_adw*/
+select 
+    *
+from customer_adw
